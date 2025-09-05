ba22cd2cbeac6132c31cca7c34c3ebef18bd413d
DJ Correa <djcorreamg@gmail.com>
2025-09-04 22:20:08 -0300

    server

--- api/mercadopago.js
+++ api/mercadopago.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import * as mercadopago from 'mercadopago';
-import { auth, db } from './firebaseAdmin';
+import { auth, db } from "./firebaseAdmin.js";
 import cors from 'cors';
 
 // ─── 1) CONFIGURAÇÃO DO EXPRESS ────────────────────────────
