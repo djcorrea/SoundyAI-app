035ebfed4a611061e09f8232f1315dd5d901f269
DJ Correa <djcorreamg@gmail.com>
2025-09-07 22:52:46 -0300

    sdfds

--- api/b2.js
+++ api/b2.js
@@ -4,19 +4,19 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const s3 = new AWS.S3({
-  endpoint: new AWS.Endpoint(`https://${process.env.B2_BUCKET_NAME}.s3.us-east-005.backblazeb2.com`), 
+  endpoint: new AWS.Endpoint(process.env.B2_ENDPOINT), // precisa https://
   region: "us-east-005",
+  s3ForcePathStyle: true,  // 👈 força path-style
   credentials: {
     accessKeyId: process.env.B2_KEY_ID,
     secretAccessKey: process.env.B2_APP_KEY,
   },
   signatureVersion: "v4",
-  s3ForcePathStyle: false, // 👈 garante virtual-host
 });
 
 // Debug
 console.log("🔑 [b2.js] Config:", {
-  endpoint: `https://${process.env.B2_BUCKET_NAME}.s3.us-east-005.backblazeb2.com`,
+  endpoint: process.env.B2_ENDPOINT,
   bucket: process.env.B2_BUCKET_NAME,
 });
 
